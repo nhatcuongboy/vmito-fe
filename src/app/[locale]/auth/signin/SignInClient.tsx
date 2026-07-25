3aa78e88d82c93b08fc97d295b4d410a93f17db7
nhatcuongboy <nhatcuongboy@gmail.com>
2026-07-26 05:01:01 +0700

    feat(signin): add Facebook login option and update translations

--- src/app/[locale]/auth/signin/SignInClient.tsx
+++ src/app/[locale]/auth/signin/SignInClient.tsx
@@ -337,6 +337,41 @@ function SignInForm() {
               {t('continueWithGoogle')}
             </Link>
 
+            {/* Facebook Login Button */}
+            <Link
+              href={`${process.env.NEXT_PUBLIC_API_URL}/auth/facebook?locale=${locale}${searchParams.get('returnUrl') ? `&returnUrl=${encodeURIComponent(searchParams.get('returnUrl')!)}` : ''}`}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap={2}
+              width="full"
+              py={3}
+              px={4}
+              borderRadius="md"
+              border="1px solid"
+              borderColor="border"
+              fontWeight="semibold"
+              color="fg"
+              bg={{ base: 'white', _dark: 'gray.700' }}
+              _hover={{
+                bg: { base: 'gray.50', _dark: 'gray.600' },
+                textDecoration: 'none',
+              }}
+            >
+              <svg
+                width="18"
+                height="18"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#1877F2"
+                  d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                />
+              </svg>
+              {t('continueWithFacebook')}
+            </Link>
+
             {/* Zalo login - temporarily hidden */}
             {/* <Link
               href={`${process.env.NEXT_PUBLIC_API_URL}/auth/zalo?locale=${locale}${searchParams.get('returnUrl') ? `&returnUrl=${encodeURIComponent(searchParams.get('returnUrl')!)}` : ''}`}
